58a7e29585e71d24fa7487e9b8b921e964b8b4eb
2BAB <xx2bab@gmail.com>
2020-09-26 22:41:41 +0800

    feature: use official api to get merged manifest instead of getting from task fields

--- polyfill-manifest/src/main/kotlin/me/xx2bab/polyfill/manifest/source/ManifestMergeOutputProvider.kt
+++ polyfill-manifest/src/main/kotlin/me/xx2bab/polyfill/manifest/source/ManifestMergeOutputProvider.kt
@@ -1,8 +1,8 @@
 package me.xx2bab.polyfill.manifest.source
 
+import com.android.build.api.artifact.ArtifactType
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.api.variant.Variant
-import com.android.build.gradle.tasks.ProcessApplicationManifest
 import me.xx2bab.polyfill.matrix.base.ApplicationSelfManageableProvider
 import org.gradle.api.Project
 import org.gradle.api.file.RegularFile
@@ -14,8 +14,9 @@ class ManifestMergeOutputProvider: ApplicationSelfManageableProvider<RegularFile
     override fun initialize(project: Project,
                             androidExtension: CommonExtension<*, *, *, *, *, *, *, *>,
                             variant: Variant) {
-        val t = project.tasks.withType(ProcessApplicationManifest::class.java)
-        mergedManifests = t.first().mergedManifest.get()
+        mergedManifests = variant.artifacts.get(ArtifactType.MERGED_MANIFEST).get()
+//        val t = project.tasks.withType(ProcessApplicationManifest::class.java)
+//        mergedManifests = t.first().mergedManifest.get()
     }
 
     override fun get(defaultValue: RegularFile?): RegularFile? {
