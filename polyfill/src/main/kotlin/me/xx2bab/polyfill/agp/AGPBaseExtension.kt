db100ef499abd771308cc00e8590c3005b67aea5
2BAB <xx2bab@gmail.com>
2022-03-13 22:02:58 +0800

    fix: toApplicationVariantImpl() throws cast error
    ApplicationVariant can be AnalyticsEnabledApplicationVariant when you add `-s`

--- polyfill/src/main/kotlin/me/xx2bab/polyfill/agp/AGPBaseExtension.kt
+++ polyfill/src/main/kotlin/me/xx2bab/polyfill/agp/AGPBaseExtension.kt
@@ -1,9 +1,22 @@
 package me.xx2bab.polyfill.agp
 
+import com.android.build.api.component.analytics.AnalyticsEnabledApplicationVariant
 import com.android.build.api.variant.Variant
 import com.android.build.api.variant.impl.ApplicationVariantImpl
 
-fun Variant.toApplicationVariantImpl() = (this as ApplicationVariantImpl)
+fun Variant.toApplicationVariantImpl() : ApplicationVariantImpl {
+    return when (this) {
+        is ApplicationVariantImpl -> {
+            this
+        }
+        is AnalyticsEnabledApplicationVariant -> {
+            this.delegate as ApplicationVariantImpl
+        }
+        else -> {
+            throw UnsupportedOperationException("Can not convert $this to ApplicationVariantImpl.")
+        }
+    }
+}
 
 fun Variant.toApkCreationConfigImpl() = toApplicationVariantImpl().delegate
 
