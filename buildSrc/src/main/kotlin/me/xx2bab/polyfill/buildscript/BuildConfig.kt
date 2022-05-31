a4e25f96888ae6c62967ba4ce84cf90ef72966eb
2BAB <xx2bab@gmail.com>
2022-05-31 22:28:46 +0800

    fix: add env preparation task

--- buildSrc/src/main/kotlin/me/xx2bab/polyfill/buildscript/BuildConfig.kt
+++ buildSrc/src/main/kotlin/me/xx2bab/polyfill/buildscript/BuildConfig.kt
@@ -12,7 +12,7 @@ object BuildConfig {
     }
 
     object Versions {
-        const val polyfillDevVersion = "0.6.2"
+        const val polyfillDevVersion = "0.7.0"
 
         val polyfillSourceCompatibilityVersion = JavaVersion.VERSION_1_8
         val polyfillTargetCompatibilityVersion = JavaVersion.VERSION_1_8
