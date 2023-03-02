f93b0397739eda4b6982a7c440446165c7b43392
2BAB <xx2bab@gmail.com>
2023-03-02 19:06:37 +0800

    release: bump to 0.8.1

--- buildSrc/src/main/kotlin/me/xx2bab/polyfill/buildscript/BuildConfig.kt
+++ buildSrc/src/main/kotlin/me/xx2bab/polyfill/buildscript/BuildConfig.kt
@@ -12,7 +12,7 @@ object BuildConfig {
     }
 
     object Versions {
-        const val polyfillDevVersion = "0.8.0"
+        const val polyfillDevVersion = "0.8.1"
 
         val polyfillSourceCompatibilityVersion = JavaVersion.VERSION_1_8
         val polyfillTargetCompatibilityVersion = JavaVersion.VERSION_1_8
