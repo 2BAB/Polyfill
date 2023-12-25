13460e1c1a25d8f1e4fac668c9a55729b8e30d3f
2BAB <xx2bab@gmail.com>
2023-12-25 16:41:45 +0800

    fix: the new publish plugin require a different command

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Keep the order as followed one may depend on previous one
-MODULE_ARRAY=('android-arsc-parser' 'android-manifest-parser' 'polyfill-backport' 'polyfill')
+MODULE_ARRAY=('android-arsc-parser' 'android-manifest-parser' 'polyfill-backport')
 for module in "${MODULE_ARRAY[@]}"
 do
 ./gradlew :"$module":publishPolyfillArtifactPublicationToSonatypeRepository
 done
-
+./gradlew :polyfill:publishPluginMavenPublicationToSonatypeRepository
 ./gradlew aggregateJars releaseArtifactsToGithub
