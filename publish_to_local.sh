4f414941c793a6c3c9d49c9bfd8f90c91505d365
2BAB <xx2bab@gmail.com>
2023-12-26 11:13:46 +0800

    fix: local release should change the command too

--- publish_to_local.sh
+++ publish_to_local.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # Keep the order as followed one may depend on previous one
-MODULE_ARRAY=('android-arsc-parser' 'android-manifest-parser' 'polyfill-backport' 'polyfill' 'polyfill-test-plugin')
+MODULE_ARRAY=('android-arsc-parser' 'android-manifest-parser' 'polyfill-backport')
 for module in "${MODULE_ARRAY[@]}"
 do
 ./gradlew clean :"$module":publishAllPublicationsToMyMavenlocalRepository
 done
+
+./gradlew clean :polyfill:publishPluginMavenPublicationToMavenLocalRepository
+./gradlew clean :polyfill-test-plugin:publishAllPublicationsToMyMavenlocalRepository
